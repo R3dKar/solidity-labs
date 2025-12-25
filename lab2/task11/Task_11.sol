e10aee607d832dfed5fd7a0f3ab4ba9f9fd880ec
R3dKar <aazz.140720045@gmail.com>
2025-12-26 00:15:21 +0300

    feat: update task11

--- lab2/task11/Task_11.sol
+++ lab2/task11/Task_11.sol
@@ -3,78 +3,77 @@ pragma solidity ^0.8.0;
 
 contract Task_11 {
     address public owner;
-
-    enum Status { Active, Paused, Closed }
-    Status public status;
-
-    constructor(uint _targetAmount) {
-        status = Status.Active;
-        targetAmount = _targetAmount;
-        owner = msg.sender;
-    }
-
-    uint public totalUserDeposits;
     uint public targetAmount;
+    
+    enum State { Active, Paused, Closed }
+    State public state;
 
-    mapping(address => uint) public userDeposits;
+    mapping(address => uint) public balances;
+    uint public totalUserDeposits;
 
     modifier onlyOwner() {
         require(owner == msg.sender, "Not the owner");
         _;
     }
 
-    modifier whenClosed() {
-        require(status == Status.Closed, "Contract is not closed");
+    modifier whenActiveOrPaused() {
+        require(state != State.Closed, "Contract is closed");
         _;
     }
 
     modifier whenActive() {
-        require(status == Status.Active, "Contract is not active");
+        require(state == State.Active, "Contract is not active");
         _;
     }
 
-    modifier whenNotClosed() {
-        require(status != Status.Closed, "Contract is closed");
+    modifier whenClosed() {
+        require(state == State.Closed, "Contract is not closed");
         _;
     }
 
     event Deposited(address indexed user, uint amount);
     event Withdrawn(address indexed user, uint amount);
-    event Closed(uint totalAmount);
+    event StateChanged(State newState);
+
+    constructor(uint _targetAmount) {
+        require(_targetAmount > 0, "Target amount should be > 0");
+        owner = msg.sender;
+        targetAmount = _targetAmount;
+        state = State.Active;
+    }
 
     function deposit() external payable whenActive {
         require(msg.value > 0, "Deposit must be greater than 0");
 
-        userDeposits[msg.sender] += msg.value;
+        balances[msg.sender] += msg.value;
         totalUserDeposits += msg.value;
 
         emit Deposited(msg.sender, msg.value);
 
         if (totalUserDeposits >= targetAmount) {
-            status = Status.Closed;
-
-            emit Closed(totalUserDeposits);
+            state = State.Closed;
+            emit StateChanged(state);
         }
     }
 
-    function withdraw() external whenNotClosed {
-        require(userDeposits[msg.sender] > 0, "No funds to withdraw");
+    function withdraw() external whenActiveOrPaused {
+        require(state == State.Paused, "Fund withdraw available only if paused");
 
-        uint amount = userDeposits[msg.sender];
+        uint amount = balances[msg.sender];
+        require(amount > 0, "No funds to withdraw");
 
         (bool success,) = msg.sender.call{value: amount}("");
         require(success, "Withdrawal failed");
 
-        userDeposits[msg.sender] = 0;
+        balances[msg.sender] = 0;
         totalUserDeposits -= amount;
 
         emit Withdrawn(msg.sender, amount);
     }
 
-    function withdrawAll() external onlyOwner whenClosed {
-        require(totalUserDeposits > 0, "No funds to withdraw");
-
-        uint amount = totalUserDeposits;
+    function ownerWithdrawAll() external onlyOwner whenClosed {
+        uint amount = address(this).balance;
+        require(amount > 0, "No funds to withdraw");
 
         (bool success,) = owner.call{value: amount}("");
         require(success, "Withdrawal failed");
@@ -84,11 +83,22 @@ contract Task_11 {
         emit Withdrawn(owner, amount);
     }
 
-    function pause() external onlyOwner whenNotClosed {
-        status = Status.Paused;
+    function pause() external onlyOwner whenActiveOrPaused {
+        require(state == State.Active, "Contract paused");
+        state = State.Paused;
+        emit StateChanged(state);
     }
 
-    function unpause() external onlyOwner whenNotClosed {
-        status = Status.Active;
+    function resume() external onlyOwner whenActiveOrPaused {
+        require(state == State.Paused, "Contract is not paused");
+        state = State.Active;
+        emit StateChanged(state);
+    }
+
+    function getState() external view returns (string memory) {
+        if (state == State.Active) return "Active";
+        if (state == State.Paused) return "Paused";
+        if (state == State.Closed) return "Closed";
+        return "";
     }
 }
