e4403efeab475914f766516c2bb75be71633ccff
R3dKar <aazz.140720045@gmail.com>
2025-12-26 01:46:50 +0300

    feat: update task9 code

--- lab2/task9/Task_09.sol
+++ lab2/task9/Task_09.sol
@@ -2,48 +2,23 @@
 pragma solidity ^0.8.0;
 
 contract Task_09 {
-    address public owner;
+    mapping(uint256 => string) public users;
+    uint256 public userCount;
 
-    constructor() {
-        owner = msg.sender;
+    event UserAdded(uint256 indexed userId, string message);
+    event UserRemoved(uint256 indexed userId, string message);
+
+    function addUser(string memory name) external {
+        userCount++;
+        users[userCount] = name;
+
+        emit UserAdded(userCount, "User added successfully");
     }
 
-    modifier only_owner() {
-        require(msg.sender == owner, "Not the owner");
-        _;
-    }
+    function removeUser(uint256 userId) external {
+        require(bytes(users[userId]).length != 0, "User  does not exist.");
+        delete users[userId];
 
-    struct User {
-        uint id;
-        string name;
-    }
-
-    mapping(uint => User) private users;
-    mapping(uint => bool) private user_exist;
-    uint private last_id;
-
-    event UserAdded(uint indexed user_id, string message);
-    event UserRemoved(uint indexed user_id, string message);
-
-    function addUser(string calldata name) external only_owner {
-        last_id++;
-        users[last_id] = User(last_id, name);
-        user_exist[last_id] = true;
-
-        emit UserAdded(last_id, "User added successfully");
-    }
-
-    function removeUser(uint user_id) external only_owner {
-        require(user_exist[user_id], "User does not exist");
-
-        users[user_id] = User(0, "");
-        user_exist[user_id] = false;
-
-        emit UserRemoved(user_id, "User removed successfully");
-    }
-
-    function getUser(uint user_id) external view returns (User memory) {
-        require(user_exist[user_id], "User does not exist");
-        return users[user_id];
+        emit UserRemoved(userId, "User removed successfully");
     }
 }
